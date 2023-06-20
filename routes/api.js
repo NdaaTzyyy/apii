a0620dac159de348c5c9edef72db959cbe43eaaf
Lu asik bang <98279739+NdaaaBilek@users.noreply.github.com>
2023-06-20 08:31:22 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,27 +3,13 @@ var scr = require("api-dylux")
 var router = express()
 
 router.get("/ai", async (req, res, next) => {
-    const { Configuration, OpenAIApi } = require("openai")
     let query = req.query.query
     if (!query) return res.json({"message":"masukkan query"})
-        const keynya = `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`
-const configuration = new Configuration({
-  apiKey: keynya,
-});
-const openai = new OpenAIApi(configuration);
-const response = await openai.createCompletion({
-              model: "text-davinci-003",
-              prompt: query,
-              temperature: 0,
-              max_tokens: 1000,
-              top_p: 1,
-              frequency_penalty: 0.2,
-              presence_penalty: 0,
-            });
-            const resultnya = response.data.choices[0].text;
-            res.json({
-                resultnya
-            })
+    const ress = await scr.ChatGpt(query)
+    var result
+    res.json({
+        result
+    })
 })
 router.get("/xnxxsearch", async (req, res, next) => {
     let query = req.query.query
